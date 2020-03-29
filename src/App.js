5e81ca2a85e76ce2464f93aaaabcd5def39178fe
sarliss <sinaramartins@gmail.com>
2020-03-29 19:19:09 -0400

    Added header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 
-// https://newsapi.org/docs/endpoints/top-headlines
+// https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+class App extends React.Component {
+
+
+
+  render() {
+    return (
+      <div className="App">
+    
+        <header>USA Top Headlines</header>
+    
+      </div>
+    );
+  }
 }
 
 export default App;
