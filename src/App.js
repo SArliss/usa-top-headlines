46005ec9de0dc2fbfbc65383148fe0a4f2b0f4cf
sarliss <sinaramartins@gmail.com>
2020-03-29 19:38:35 -0400

    Added axios and fetched news data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 // https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      news: [],
+      selectedNew: [],
+      isLoading: false,
+      error: ""
+    }
+  }
 
+  async componentDidMount() {
+    this.setState({ isLoading: true });
+
+    try {
+      const news = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152');
+      console.log(news.data)
+      this.setState({
+        news: news.data,
+        isLoading: false
+      });
+    } catch (error) {
+      console.log(error.message)
+      this.setState({
+        error: error.message,
+        isLoading: false
+      });
+    }
+  }
 
 
   render() {
     return (
       <div className="App">
-    
+
         <header>USA Top Headlines</header>
-    
+
+        <div className="news-container">
+          
+        </div>
+
       </div>
     );
   }
