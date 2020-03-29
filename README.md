dcac11c40ef419957201f03de61cf761c12d04e7
sarliss <sinaramartins@gmail.com>
2020-03-29 18:20:40 -0400

    Created react app

---
+++ README.md
@@ -0,0 +1,3 @@
+## News
+
+This project was build with create-react-app and newsapi.org to display the latest USA top news.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+// https://newsapi.org/docs/endpoints/top-headlines
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
