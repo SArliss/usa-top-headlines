f047821e310e8a81bd80f7a50686d1bafc55d0c2
sarliss <sinaramartins@gmail.com>
2020-03-30 00:56:02 -0400

    Rendered news and added style

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      news: [],
+      news: {},
       selectedNew: [],
       isLoading: false,
-      error: ""
+      error: "",
+      curTime: new Date().toLocaleString()
     }
   }
 
@@ -20,7 +21,6 @@ class App extends React.Component {
 
     try {
       const news = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152');
-      console.log(news.data)
       this.setState({
         news: news.data,
         isLoading: false
@@ -36,14 +36,33 @@ class App extends React.Component {
 
 
   render() {
+    const news = this.state.news.articles;
+    console.log(news);
+
     return (
       <div className="App">
 
-        <header>USA Top Headlines</header>
+        <header>
+          <h1>America Top News</h1>
+          <p>{this.state.curTime}</p>
+          <p>COVID-19 news: <a href="https://www.worldometers.info/coronavirus/country/us/">See the latest coverage of the new coronavirus</a></p>
+        </header>
 
         <div className="news-container">
-          
+
+          {this.state.isLoading && <p>Loading news...</p>}
+          {this.state.error && <p>{this.state.error}</p>}
+
+          {news && news.map((noticia, index) =>
+            <div key={index} className="single-new">
+              <h4>{noticia.source.name.toUpperCase()}</h4>
+              <img src={noticia.urlToImage} alt={noticia.source.name} />
+              <p>{noticia.title}.</p>
+            </div>
+          )}
         </div>
+
+        <footer>Copyright © Sinara Arliss 2020</footer>
 
       </div>
     );
