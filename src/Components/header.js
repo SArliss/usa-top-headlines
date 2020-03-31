03367902316dc6690bcb928c5f7a6a4ff44a4b79
sarliss <sinaramartins@gmail.com>
2020-03-30 21:23:40 -0400

    [header]Fixed typo

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
   return (
     <header>
-      <h1>America Top News</h1>
+      <h1>America's Top News</h1>
       <p>{props.curTime}</p>
       <p>COVID-19 news: <a href="https://www.worldometers.info/coronavirus/country/us/" target="_blank" rel="noreferrer">See the latest coverage of the new coronavirus</a></p>
     </header>
