a63ffe94e280da72a34ff3f1d457fa5da0617bd3
sarliss <sinaramartins@gmail.com>
2020-06-07 01:30:22 -0400

    Updated api-helper with cors-anywhere.herokuapp

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ class App extends React.Component {
     this.setState({ isLoading: true });
 
     try {
-      // const news = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152');
       const news = await getNews();
 
       this.setState({
--- src/Services/api-helper.js
+++ src/Services/api-helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const getNews = async () => {
-  const news = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152');
+  const news = await axios.get('https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152');
   return news;
 }
 
