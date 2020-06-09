d8cd3e46d5a32d13aa425ae94798dc6f4dc82c44
sarliss <sinaramartins@gmail.com>
2020-06-08 21:51:14 -0400

    Decreased single-new size to 25em

--- src/Services/api-helper.js
+++ src/Services/api-helper.js
@@ -1,27 +1,8 @@
-// import axios from 'axios';
-
-// const getNews = async () => {
-//   const news = await axios.get('https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152');
-//   return news;
-// }
-
-// export default getNews;
-
 import axios from 'axios';
 
-export async function getNews() {
-    try {
-    const response = await axios({
-      method: 'GET',
-      url: 'https://yacdn.org/proxy/https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152',
-      // url: 'https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152',
-    });
-      console.log(response);
-      return response;
-  }
-  catch (error) {
-    console.log(error);
-  }
+const getNews = async () => {
+  const news = await axios.get('https://yacdn.org/proxy/https://newsapi.org/v2/top-headlines?country=us&apiKey=492d060bf37d4e54bf1ec609e650f152');
+  return news;
 }
 
 export default getNews;
